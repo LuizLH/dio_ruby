0de2bd7e016ca576b1d51beffdf339d473a91a58
LuizLH <luizlh@gmail.com>
2022-12-17 22:59:11 -0300

    test: test

--- 3_listas.rb
+++ 3_listas.rb
@@ -36,17 +36,31 @@ puts b.length
 puts b[0][1]
 =end
 
-def busy_days(birds_per_day)
-    ct = 0
-    birds_per_day.each do | d |
-      if d >= 5
-        ct += 1
-      end
-    end
-    ct
-end
 
-puts busy_days([2, 5, 0, 7, 4, 1])
+items = [{ price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4} },
+          { price: 50.00, name: "Red Short Skirt", quantity_by_size: {} },
+          { price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4} },
+          { price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2} }
+        ]
+
+# arr.sum {|h| h[:amount] }
+# array.map { |h| h[:amount] }.sum
+# total = 0
+# @items.each { |item| item[:quantity_by_size][:]  }
+# total
+items.each do |item|
+  # puts item[:quantity_by_size][:s]
+  # puts item[:quantity_by_size][:m]
+  # puts item[:quantity_by_size][:l]
+  # puts item[:quantity_by_size][:xl]
+  # puts item[:quantity_by_size][:s] + item[:quantity_by_size][:m] + item[:quantity_by_size][:l] + item[:quantity_by_size][:xl]
+  total = 0
+  # item[:quantity_by_size].each { |subhash| subhash.each { |_prod, value| total += !value.nil? ? value : 0 } }
+  # item[:quantity_by_size].map { |subhash| subhash.each { |prod, value| puts "#{prod} - #{value}" } }
+
+  item[:quantity_by_size].map { |h| h.empty? ? 0 : h[:s].to_i + h[:m].to_i + [:l].to_i + [:xl].to_i }.sum
+  puts total
+end
 
 # b.each do |item, idx|
 #  puts idx
